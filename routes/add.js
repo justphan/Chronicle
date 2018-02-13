9f92be46f0306f7dad6845ed314fa5f8d8ac4aa6
Justin Phan <juphan@ucsd.edu>
2018-02-12 16:18:25 -0800

    data.json prototype and addEvent addCategory prototypes

---
+++ routes/add.js
@@ -0,0 +1,25 @@
+var data = require("../data.json");
+
+exports.addEvent = function(req, res) {
+	var event = {
+		"category": req.query.category,
+		"name": if (req.query.name) == "" {
+			req.query.category;
+		}
+		else {
+			req.query.name;
+		},
+		"time": req.query.time,
+		"date": req.query.date
+	};
+	data.events.push(event);
+	res.render('index', data);
+}
+
+exports.addCategory = function(req, res) {
+	var category = {
+		"name": req.query.name
+	};
+	data.categories.push(category);
+	res.render('settings');
+}
