c1c4aeb4e4fd094b85f3a555e423cfbc633ace9a
Hunter Lai <1hunterlai@gmail.com>
2018-02-14 15:20:17 -0800

    add js file

---
+++ public/js/calendar.js
@@ -0,0 +1,16 @@
+var data = 	require("../data.json");
+
+
+
+//COMMENTED OUT UNTIL WE FIGURE OUT DATABASE
+
+// exports.addEvent = function(req, res) {
+// 	var newEvent = {
+
+// 	}
+
+// 	console.log(newEvent);
+// 	data.events.push(newEvent);
+
+// 	res.render("calendar", data)
+// }
