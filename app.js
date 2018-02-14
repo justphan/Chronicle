4f90289cef24af047ca3fa777c5b5d0dd963c2ed
Hunter Lai <1hunterlai@gmail.com>
2018-02-14 15:40:12 -0800

    renamed file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var calendar = require('./routes')
 // Example route
 // var user = require('./routes/user');
 var add = require('./routes/add');
