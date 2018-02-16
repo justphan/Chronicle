c634d57e4dbdbefd18e1bac605a6a7a80d2affc9
Franklin Li <frl003@ucsd.edu>
2018-02-15 21:36:07 -0800

    addevent.js test and commented out

--- routes/addevent.js
+++ routes/addevent.js
@@ -2,6 +2,20 @@ var data = require("../accounts.json");
 
 exports.view = function(req,res){
 	console.log(data);
-
+	req.query.category;
+	req.query.name;
+	req.query.timestart;
+	req.query.ampm
+	req.query.timeend;
+	/*var newEvent = {
+		"category": req.query.category,
+		"name": req.query.name,
+		"timestart": req.query.timestart,
+		"timeend": req.query.timeend,
+		"date": 
+	};
+	console.log(newEvent);
+	data.accounts.events.push(newEvent);*/
 	res.render('addevent', data);
+
 }
