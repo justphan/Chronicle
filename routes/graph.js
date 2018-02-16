7ff997e679dc0e33e9953c4ac80807f1ae115157
Justin Phan <juphan@ucsd.edu>
2018-02-15 23:15:46 -0800

    Graph displays based off data

--- routes/graph.js
+++ routes/graph.js
@@ -3,5 +3,6 @@ var data = require("../accounts.json");
 exports.view = function(req,res){
 	console.log(data);
 
-	res.render('graph', data);
+	//res.render('graph', data);
+	res.render('graph', {encodedJson : encodeURIComponent(JSON.stringify(data))});
 }
