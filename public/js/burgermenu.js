5d88af98fb566e0722634beb2209f598cbf72c6e
Hunter Lai <1hunterlai@gmail.com>
2018-03-05 20:50:09 -0800

    hamburger button works now

--- public/js/burgermenu.js
+++ public/js/burgermenu.js
@@ -5,7 +5,9 @@ $( document ).ready(function() {
 
 $("body").on('click', '.top', function() {
 		$("nav.menu").toggleClass("menu_show");
+		console.log("click registered");
 	});
+
 
 
  $('#datePicker').val(new Date());
